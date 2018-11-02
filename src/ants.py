7030f82bd03f21ad43b1910bbaf869783f55aeaa
Ronaldo Vieira <ronaldoesv@gmail.com>
2018-11-02 11:12:09 -0300

    Allows early termination of execution

--- src/ants.py
+++ src/ants.py
@@ -9,80 +9,83 @@ class Colony:
 		self.E = edges
 
 	def run(self, **params):
-		if 'seed' in params:
-			np.random.seed(params['seed'])
+		try:
+			solutions = []
+			info = []
 
-		V, E, N = self.V, self.E, max(self.V)
-		ph = E.where(E.isnull(), 1)
-		fitness = E
+			if 'seed' in params:
+				np.random.seed(params['seed'])
 
-		solutions = []
-		info = []
+			V, E, N = self.V, self.E, max(self.V)
+			ph = E.where(E.isnull(), 1)
+			fitness = E
 
-		best_soln = (- float('inf'), [])
+			best_soln = (- float('inf'), [])
 
-		for i in range(params['max_iter']):
-			best_local_soln = (- float('inf'), [])
+			for i in range(params['max_iter']):
+				best_local_soln = (- float('inf'), [])
 
-			probs = ph ** params['alpha'] + fitness ** params['beta']
+				probs = ph ** params['alpha'] + fitness ** params['beta']
 
-			ants = []
-			repeated_edges = defaultdict(lambda: -1)
+				ants = []
+				repeated_edges = defaultdict(lambda: -1)
 
-			for k in range(params['ants']):
-				soln = [1]
+				for k in range(params['ants']):
+					soln = [1]
 
-				possible = probs.loc[soln[-1]]
-				possible = possible[~possible.index.isin(soln + [N])]
-
-				while not possible.empty:
-					v = possible.sample(weights = 'weight').index.values[0]
-
-					soln.append(v)
-
-					possible = probs.loc[v]
+					possible = probs.loc[soln[-1]]
 					possible = possible[~possible.index.isin(soln + [N])]
 
-				while N not in probs.loc[soln[-1]].index:
-					soln.pop()
+					while not possible.empty:
+						v = possible.sample(weights = 'weight').index.values[0]
 
-				soln.append(N)
+						soln.append(v)
 
-				selected_edges = list(zip(soln, soln[1:]))
-				cost = E[E.index.isin(selected_edges)].sum()['weight']
+						possible = probs.loc[v]
+						possible = possible[~possible.index.isin(soln + [N])]
 
-				for edge in selected_edges:
-					repeated_edges[edge] += 1
+					while N not in probs.loc[soln[-1]].index:
+						soln.pop()
 
-				ants.append((selected_edges, cost))
+					soln.append(N)
 
-				if cost > best_local_soln[0]:
-					best_local_soln = (cost, list(selected_edges))
+					selected_edges = list(zip(soln, soln[1:]))
+					cost = E[E.index.isin(selected_edges)].sum()['weight']
 
-				if cost > best_soln[0]:
-					best_soln = (cost, list(selected_edges))
+					for edge in selected_edges:
+						repeated_edges[edge] += 1
 
-			solutions.append(pd.DataFrame(ants, 
-				columns = ['soln', 'cost']))
+					ants.append((selected_edges, cost))
 
-			info.append({
-				'best': solutions[-1]['cost'].max(),
-				'mean': solutions[-1]['cost'].mean(),
-				'std': solutions[-1]['cost'].std(),
-				'size': solutions[-1]['soln'].apply(len).mean() + 1,
-				'worst': solutions[-1]['cost'].min(),
-				'repeated_edges': sum(list(repeated_edges.values()))}
-			)
+					if cost > best_local_soln[0]:
+						best_local_soln = (cost, list(selected_edges))
 
-			in_local_best = ph.index.isin(best_local_soln[1])
-			in_global_best = ph.index.isin(best_soln[1])
+					if cost > best_soln[0]:
+						best_soln = (cost, list(selected_edges))
 
-			ph *= 1 - params['evap']
+				solutions.append(pd.DataFrame(ants, 
+					columns = ['soln', 'cost']))
 
-			ph[in_local_best] += params['Q'] * best_local_soln[0]
-			ph[in_global_best] += params['Q'] * best_soln[0]
+				info.append({
+					'best': solutions[-1]['cost'].max(),
+					'mean': solutions[-1]['cost'].mean(),
+					'std': solutions[-1]['cost'].std(),
+					'size': solutions[-1]['soln'].apply(len).mean() + 1,
+					'worst': solutions[-1]['cost'].min(),
+					'repeated_edges': sum(list(repeated_edges.values()))}
+				)
 
-			print(best_soln[0], best_local_soln[0], solutions[-1]['cost'].mean())
-			print(probs.mean()['weight'], probs.max()['weight'], probs.min()['weight'])
+				in_local_best = ph.index.isin(best_local_soln[1])
+				in_global_best = ph.index.isin(best_soln[1])
+
+				ph *= 1 - params['evap']
+
+				ph[in_local_best] += params['Q'] * best_local_soln[0]
+				ph[in_global_best] += params['Q'] * best_soln[0]
+
+				print(best_soln[0], best_local_soln[0], solutions[-1]['cost'].mean())
+				print(probs.mean()['weight'], probs.max()['weight'], probs.min()['weight'])
+		except KeyboardInterrupt:
+			print("Stopping")
 
 		return solutions, info
